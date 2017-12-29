7a3fcb78cfd16074dbf7e1bea018d5b215820e17
Jay <qiu.jay892@gmail.com>
2017-12-29 11:59:40 +0800

    更新 file provider,防止冲突

---
+++ daguerre/src/main/java/com/jay/daguerre/provider/DaguerreFileProvider.java
@@ -0,0 +1,9 @@
+package com.jay.daguerre.provider;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by jay on 2017/12/29 上午11:57
+ */
+public class DaguerreFileProvider extends FileProvider {
+}
